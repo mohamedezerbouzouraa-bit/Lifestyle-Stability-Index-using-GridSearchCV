29509d636acf871d10c10f7c26537e4063a87d00
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-09 17:42:25 +0100

    Trying to find the best model at point of score then using it

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from src.user_interaction import get_user_input, predict_user_stability
 
 X, y = load_data()
 best_model, gs, X_test, y_test = train_model(X, y)
-
 print("Best CV Accuracy:", gs.best_score_)
 print("Best Hyperparameters:", gs.best_params_)
 print("Test Accuracy:", best_model.score(X_test, y_test))
